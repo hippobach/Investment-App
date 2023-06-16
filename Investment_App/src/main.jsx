8b4b7def5927c2d541da6c5f0940ab3e0497421b
Nguyen Xuan Bach <nguyenxuanbach03nd@gmail.com>
2023-06-16 13:54:28 +0700

    fix: optimize and handle css code with scss

--- Investment_App/src/main.jsx
+++ Investment_App/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
 import GlobalStyles from './components/GlobalStyles/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(

986321700e7ff9a47f8cfa40778e839445d53da6
Nguyen Xuan Bach <nguyenxuanbach03nd@gmail.com>
2023-06-16 23:27:11 +0700

    fix: modify css format

--- Investment_App/src/App.jsx
+++ Investment_App/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
             <Header />
             <UserInput onCalculate={calculateHandler} />
 
-            {!userInput && <p>No investment calculated yet.</p>}
+            {!userInput && <p style={{ textAlign: 'center' }}>No investment calculated yet.</p>}
             {userInput && <ResultsTable data={yearlyData} initialInvestment={userInput['current-savings']} />}
         </div>
     );
